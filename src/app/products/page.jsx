6a191cf34df78f0dd10d05dfd504ace45358cc00
NRS\nrsha <nrshakib007@gmail.com>
2024-05-10 12:06:08 +0600

    Dynamic Routes

---
+++ src/app/products/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1>Product List</h1>
+      <h3>Product 1</h3>
+      <h3>Product 2</h3>
+      <h3>Product 3</h3>
+    </div>
+  );
+};
+
+export default ProductList;
