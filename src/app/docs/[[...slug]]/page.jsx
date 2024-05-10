29e22bb9f72e04ec5ea2a42176cc3e5f8d9a281e
NRS\nrsha <nrshakib007@gmail.com>
2024-05-10 12:50:05 +0600

    Catch All Segments

---
+++ src/app/docs/[[...slug]]/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Docs = ({ params }) => {
+  {
+    if (params.slug?.length === 2) {
+      return (
+        <h1>
+          Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
+        </h1>
+      );
+    } else if (params.slug?.length === 1) {
+      return <h1>Viewing docs for feature {params.slug[0]}</h1>;
+    }
+  }
+  return <div>Docs Home Page</div>;
+};
+
+export default Docs;
