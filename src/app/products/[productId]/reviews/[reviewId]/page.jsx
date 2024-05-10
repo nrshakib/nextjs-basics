51881b1acb8c235db56dcc810dae16a775b039e6
NRS\nrsha <nrshakib007@gmail.com>
2024-05-10 17:09:18 +0600

    Custom Not Found Page

--- src/app/products/[productId]/reviews/[reviewId]/page.jsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.jsx
@@ -1,6 +1,10 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 const ReviewDetails = ({ params }) => {
+  if (parseInt(params.reviewId > 1000)) {
+    notFound();
+  }
   return (
     <div>
       Review {params.reviewId} for product {params.productId}
